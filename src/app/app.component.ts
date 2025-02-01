6493c204ec01887073264438554b70ca52b4deb4
KevDil <k.dillhardt@gmail.com>
2025-02-01 20:24:59 +0100

    add caseking in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { FoundersEditionComponent } from './shops/founders-edition/founders-edition.component';
 import { AlternateComponent } from "./shops/alternate/alternate.component";
+import { CasekingComponent } from "./shops/caseking/caseking.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FoundersEditionComponent, AlternateComponent],
+  imports: [FoundersEditionComponent, AlternateComponent, CasekingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
