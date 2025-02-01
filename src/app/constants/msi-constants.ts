97b4d297d3c79f2e76afb1ef395c98bb6dc5a283
KevDil <k.dillhardt@gmail.com>
2025-02-01 21:55:03 +0100

    add msi

---
+++ src/app/constants/msi-constants.ts
@@ -0,0 +1,24 @@
+export const URLS = {
+  'MSI GeForce RTX 5080 16G GAMING TRIO OC':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-gaming-trio-oc',
+  'MSI GeForce RTX 5080 16G VENTUS 3X OC PLUS':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-ventus-3x-oc-plus-1',
+  'MSI GeForce RTX 5080 16G SUPRIM LIQUID SOC ':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-suprim-liquid-soc',
+  'MSI GeForce RTX 5080 16G SUPRIM SOC':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-suprim-soc',
+  'MSI GeForce RTX 5080 16G VANGUARD SOC LAUNCH EDITION':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-gaming-trio-oc',
+  'MSI GeForce RTX 5080 16G GAMING TRIO OC WHITE':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-gaming-trio-oc-white',
+  'MSI GeForce RTX 5080 16G VENTUS 3X OC':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5080-16g-ventus-3x-oc',
+  'MSI GeForce RTX 5090 32G SUPRIM SOC':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5090-32g-suprim-soc',
+  'MSI GeForce RTX 5090 32G GAMING TRIO OC':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/geforce-rtx-5090-32g-gaming-trio-oc',
+  'MSI GeForce RTX 5090 32G VENTUS 3X OC':
+    'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/msi-grafikkarte-rtx-5090-ventus-3x-oc',
+    'MSI Grafikkartenhalter':
+      'https://de-store.msi.com/collections/geforce-rtx-50-series-gpu/products/card-holder'
+};
