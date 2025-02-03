d7ba3fe2b2838edf66083d7b4483c2ab9a43b0b8
KevDil <k.dillhardt@gmail.com>
2025-02-03 12:06:43 +0100

    add cu url

--- src/app/constants/computeruniverse-constants.ts
+++ src/app/constants/computeruniverse-constants.ts
@@ -1,6 +1,8 @@
 export const URLS = {
   'GIGABYTE GeForce RTX 5080 WINDFORCE OC SFF':
     'https://www.computeruniverse.net/de/p/2E21-592',
+  'INNO3D GeForce RTX 5080 X3 OC':
+    'https://www.computeruniverse.net/de/p/2E30-03Q',
   'ASUS DUAL GeForce RTX 3060 V2 OC':
     'https://www.computeruniverse.net/de/p/2E07-554',
 };
@@ -8,6 +10,8 @@ export const URLS = {
 export const APIS = {
   'GIGABYTE GeForce RTX 5080 WINDFORCE OC SFF':
     'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E21-592"]}',
+  'INNO3D GeForce RTX 5080 X3 OC':
+    'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E30-03Q"]}',
   'ASUS DUAL GeForce RTX 3060 V2 OC':
     'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E07-554"]}',
 };
