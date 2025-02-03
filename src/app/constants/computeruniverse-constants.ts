726b4a06672ee21498b14cf2dab007209dbb61f9
KevDil <k.dillhardt@gmail.com>
2025-02-03 13:48:58 +0100

    add cu url

--- src/app/constants/computeruniverse-constants.ts
+++ src/app/constants/computeruniverse-constants.ts
@@ -3,6 +3,8 @@ export const URLS = {
     'https://www.computeruniverse.net/de/p/2E21-592',
   'INNO3D GeForce RTX 5080 X3 OC':
     'https://www.computeruniverse.net/de/p/2E30-03Q',
+  'GIGABYTE GeForce RTX 5080 AERO OC SFF':
+    'https://www.computeruniverse.net/de/p/2E21-591',
   'ASUS DUAL GeForce RTX 3060 V2 OC':
     'https://www.computeruniverse.net/de/p/2E07-554',
 };
@@ -12,6 +14,8 @@ export const APIS = {
     'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E21-592"]}',
   'INNO3D GeForce RTX 5080 X3 OC':
     'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E30-03Q"]}',
+  'GIGABYTE GeForce RTX 5080 AERO OC SFF':
+    'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E21-591"]}',
   'ASUS DUAL GeForce RTX 3060 V2 OC':
     'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E07-554"]}',
 };
