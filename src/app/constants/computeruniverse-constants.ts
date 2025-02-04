50c92a19cd5fbff2eedcc80555f39e87ae01d302
KevDil <k.dillhardt@gmail.com>
2025-02-04 13:13:53 +0100

    remove cu test url

--- src/app/constants/computeruniverse-constants.ts
+++ src/app/constants/computeruniverse-constants.ts
@@ -4,9 +4,7 @@ export const URLS = {
   'INNO3D GeForce RTX 5080 X3 OC':
     'https://www.computeruniverse.net/de/p/2E30-03Q',
   'GIGABYTE GeForce RTX 5080 AERO OC SFF':
-    'https://www.computeruniverse.net/de/p/2E21-591',
-  'ASUS DUAL GeForce RTX 3060 V2 OC':
-    'https://www.computeruniverse.net/de/p/2E07-554',
+    'https://www.computeruniverse.net/de/p/2E21-591'
 };
 
 export const APIS = {
@@ -15,7 +13,5 @@ export const APIS = {
   'INNO3D GeForce RTX 5080 X3 OC':
     'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E30-03Q"]}',
   'GIGABYTE GeForce RTX 5080 AERO OC SFF':
-    'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E21-591"]}',
-  'ASUS DUAL GeForce RTX 3060 V2 OC':
-    'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E07-554"]}',
+    'https://st-eu.dynamicyield.com/spa/json?sec=9876231&ctx={"lng":"de","type":"PRODUCT","data":["2E21-591"]}'
 };
